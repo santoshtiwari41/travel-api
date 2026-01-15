ce8cf5fd2d090e1a697e843767b012428dade50c
santoshtiwari41 <santoshhh066@gmail.com>
2026-01-15 21:12:36 +0545

    login service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,16 +2,30 @@ import { eq } from "drizzle-orm";
 import { db } from "../db/drizzle.js"
 import { users } from "../db/schema/user.js"
 import { AppError } from "../utils/appError.js";
-import { hashPassword } from "../lib/password-hash.js";
+import { comparePassword, hashPassword } from "../lib/password-hash.js";
 
-export const loginUser = async (email: string, password: string, fullName: string) => {
+export const loginUser = async (email: string, password: string) => {
+  
+    const [user] = await db.select()
+        .from(users)
+        .where(eq(users.email, email.toLowerCase().trim()))
+        .limit(1);
 
+    if (!user) {
+        throw new AppError('Invalid email or password', 401);
+    }
 
+    const isPasswordValid = await comparePassword(password, user.password);
+    
+    if (!isPasswordValid) {
+        throw new AppError('Invalid email or password', 401);
+    }
 
-}
+    return user;
+};
 
 export const signUpUser = async (email: string, password: string, fullName: string) => {
-   const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
+    const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
     if (existingUser.length > 0) {
         throw new AppError('User with this email already exists', 400);
     }
