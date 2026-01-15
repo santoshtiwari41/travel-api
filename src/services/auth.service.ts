edd032c510325469b3cc14e7fa425b19d641f17a
santoshtiwari41 <santoshhh066@gmail.com>
2026-01-15 20:55:33 +0545

    has password in registering user

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,6 +2,7 @@ import { eq } from "drizzle-orm";
 import { db } from "../db/drizzle.js"
 import { users } from "../db/schema/user.js"
 import { AppError } from "../utils/appError.js";
+import { hashPassword } from "../lib/password-hash.js";
 
 export const loginUser = async (email: string, password: string, fullName: string) => {
 
@@ -10,13 +11,12 @@ export const loginUser = async (email: string, password: string, fullName: strin
 }
 
 export const signUpUser = async (email: string, password: string, fullName: string) => {
-
-    const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
-
+   const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
     if (existingUser.length > 0) {
         throw new AppError('User with this email already exists', 400);
     }
-    const [res] = await db.insert(users).values({ email, password, fullName }).returning();
+    const newPassword = await hashPassword(password);
+    const [res] = await db.insert(users).values({ email, password: newPassword, fullName }).returning();
     return res;
 
 }
