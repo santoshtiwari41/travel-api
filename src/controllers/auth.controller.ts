4fc6f78c25f772ee368271c7bf3def4aa0254ce8
santoshtiwari41 <santoshhh066@gmail.com>
2026-01-15 21:20:43 +0545

    login route and controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { AppError } from "../utils/appError.js";
-import { signUpUser } from "../services/auth.service.js";
+import { loginUser, signUpUser } from "../services/auth.service.js";
 import { generateToken } from "../lib/jwt.js";
 
 export async function Register(req:Request, res:Response) {
@@ -32,4 +32,32 @@ export async function Register(req:Request, res:Response) {
     console.log('error is ',error)
     return res.status(500).json({message: "Internal Server Error",error});
 }
+}
+
+
+export async function Login(req:Request, res:Response) {
+
+    try{
+        const {email,password}=req.body;
+
+        if(!email || !password){
+            return res.status(400).json({message:"Email and password are required "})
+        }
+
+        const data=await loginUser(email,password);
+        if(!data){
+            return res.status(400).json({message:"failed to login user"});
+        }
+         const token =generateToken({userId:data.id,email},'10m');
+        return res.status(200).json({message:"User logged in successfully",token});
+           
+
+    }
+
+    catch(error){
+        if(error instanceof AppError){
+            return res.status(error.statusCode).json({message: error.message});
+        }
+        return res.status(500).json({message: "Internal Server Error"});
+    }
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { Register } from "../controllers/auth.controller.js";
+import { Login, Register } from "../controllers/auth.controller.js";
 
 
 const router:Router = Router();
-// router.get('/login',)
+router.post('/login',Login)
 router.post('/register',Register)
 // router.post('/forgot-password')
 // router.post('/reset-password')
