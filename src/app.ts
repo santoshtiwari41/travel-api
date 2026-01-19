65bb8db911e0b6baacee56a627958e0ffa14983b
santoshtiwari41 <santoshhh066@gmail.com>
2026-01-19 14:25:22 +0545

    fix cors and route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import type { Express } from 'express';
+import cors from 'cors'
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 export const app:Express=express();
-
+app.use(cors())
 app.use(express.json());
 app.get('/', (_req, res) => {
   res.send('Travel API is running ');
 });
 
-app.use('/api/auth',authRoutes)
-app.use('/api/users',userRoutes)
+app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/users',userRoutes)
