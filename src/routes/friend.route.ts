93349a324f884f4a1c4c6d0c45a6c92aaac1dfda
santoshtiwari41 <santoshhh066@gmail.com>
2026-02-01 13:00:30 +0545

    friend routes

--- src/routes/friend.route.ts
+++ src/routes/friend.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { acceptRequest, sendRequest } from "src/controllers/friend.controller.js";
+import { acceptRequest, getAllRequest, getFriends, sendRequest,rejectRequest } from "src/controllers/friend.controller.js";
 import { AuthMiddleware } from "src/middlewares/auth.middleware.js";
 
 const router:Router = Router();
-
+router.get('/',AuthMiddleware,getFriends)
+router.get('/request',AuthMiddleware,getAllRequest)
 router.post("/request", AuthMiddleware, sendRequest);
 router.post("/request/:requestId/accept", AuthMiddleware, acceptRequest);
+router.post("/request/:requestId/reject", AuthMiddleware, rejectRequest);
 
 export default router;
